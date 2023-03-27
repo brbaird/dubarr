397a2cbc36a947e89736cda3899c9ad85cfa8682
Braden Baird <70188874+brbaird@users.noreply.github.com>
2023-03-27 13:21:46 -0600

    Initial commit

---
+++ example_config.py
@@ -0,0 +1,3 @@
+host_url = 'https://example.sonarr.com'  # Host URL of your Sonarr instance
+api_key = 'Sonarr API Key'  # API key from Sonarr
+anime_only = True  # Whether or not to only search shows of type Anime for dubs
